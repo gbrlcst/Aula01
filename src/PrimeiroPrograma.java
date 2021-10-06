b38267c18e303056d3cc0c8bb78eb8580feb3b1b
Gabriel Costa <gbrlcst148@gmail.com>
2021-10-06 20:18:23 -0300

    Primeira versão da aula 01

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!\n");
+		System.out.print("Outra mensagem");
+
+	}
+
+}
